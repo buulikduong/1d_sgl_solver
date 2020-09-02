1266a6eebe083d772fc5b39fb60161dc0b963edf
Buulik Duong <buulik@uni-bremen.de>
2020-09-02 13:09:03 +0200

    Correction of data types of iomodule

--- modules/in_and_out.py
+++ modules/in_and_out.py
@@ -1,39 +1,39 @@
-""" Empty module for reading input data and saving output data """
+""" Module/routine for reading input data and saving output data """
 
 import sys
 import numpy as np
 
-def read_inp(file):
+def read_inp(path):
     """Reading an input file schrodinger.inp
 
     Args:
-        file: Path to the input file
+        path: Path to the input file
 
     Retruns:
-        dict: Dictionary containing all parameter from the input file
+        data: Dictionary containing all parameter from the input file
     """
 
     data = {}
 
     try:
-        fp = open(file + 'schrodinger.inp', 'r')
+        with open(path + 'schrodinger.inp', 'r') as fp:
     except OSError:
         print("Input file can not be read.\nPlease check location and permission rights.")
         sys.exit(1)
     try:
         line = fp.readline()
-        data['mass'] = line.split()[0]
+        data['mass'] = float(line.split()[0])
         line = fp.readline()
-        data['xMin'] = line.split()[0]
-        data['xMax'] = line.split()[1]
-        data['nPoint'] = line.split()[2]
+        data['xMin'] = float(line.split()[0])
+        data['xMax'] = float(line.split()[1])
+        data['nPoint'] = int(line.split()[2])
         line = fp.readline()
-        data['first'] = line.split()[0]
-        data['last'] = line.split()[1]
+        data['first'] = int(line.split()[0])
+        data['last'] = int(line.split()[1])
         line = fp.readline()
         data['interpol_method'] = line.split()[0]
         line = fp.readline()
-        data['interpol_num'] = line.split()[0]
+        data['interpol_num'] = int(line.split()[0])
         lines = np.loadtxt(fp.readlines()[0:])
         data['x_decl'] = lines[:, 0]
         data['y_decl'] = lines[:, 1]
@@ -41,5 +41,3 @@ def read_inp(file):
         print("schrodinger.inp do not have the correct format.\nPlease check your input file.")
     print(data)
     return data
-
-read_inp('/home/dbuulik/1d_sgl_solver/application_examples/infinite_potential_well/')
