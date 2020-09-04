bb7322befbb2fb71181171b6d042139b3ceb3e6c
Jarik Bahrenburg <jarik@uni-bremen.de>
2020-09-04 18:37:03 +0200

    Added interpolator

---
+++ modules/interpolator.py
@@ -0,0 +1,29 @@
+"""Module/Routine interpolating matematical functions out of support points"""
+
+from scipy.interpolate import interp1d, lagrange, CubicSpline
+
+
+def interpolator(x_sup, y_sup, method):
+    """
+    intfunction used to interpolate a mathematical function from a given set of
+    points using either linear, polynomial or cubic spline for the
+    interpolation.
+
+    Args:
+        x_sup (list): x-coordinates of the function
+        y_sup (list): y-coordinates of the function
+        method (string): name of the interpolation method to be used
+
+    Returns:
+        intfunc: interpolated function
+    """
+
+    if method == "linear":
+        intfunc = interp1d(x_sup, y_sup, kind="linear")
+        return intfunc
+    elif method == "polynomial":
+        intfunc = lagrange(x_sup, y_sup)
+        return intfunc
+    elif method == "cspline":
+        intfunc = CubicSpline(x_sup, y_sup, bc_type="natural")
+        return intfunc
